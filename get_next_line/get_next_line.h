9fd8dcd5c85858d41672f545541fcf539ff57c5c
sy33002 <113359008+sy33002@users.noreply.github.com>
2023-08-06 04:56:46 +0000

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jihyuki2 <jihyuki2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 15:57:41 by jihyuki2          #+#    #+#             */
-/*   Updated: 2023/08/01 20:53:12 by jihyuki2         ###   ########seoul.kr  */
+/*   Updated: 2023/08/03 20:26:04 by jihyuki2         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+// #define BUFFER_SIZE 4
+
 char			*get_next_line(int fd);
 char			*get_next_line(int fd);
 unsigned int	ft_strlen(char *str);
 char			*ft_strjoin(char *s1, char *s2);
 char			*ft_strchr(char *s, int c);
 char			*ft_substr(char *s, int start, int len);
+char			*put_null(char *str);
 
 #endif
